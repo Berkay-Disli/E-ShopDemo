c3b406e2474d2f2c045fa1018de6975de652fd92
Berkay-Disli <berkaydisli.work@gmail.com>
2022-06-15 13:20:19 +0300

    Initial Commit

---
+++ CampaignDemo/CampaignDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampaignDemoApp.swift
+//  CampaignDemo
+//
+//  Created by Berkay Disli on 15.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CampaignDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampaignDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CampaignDemo
+//
+//  Created by Berkay Disli on 15.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
